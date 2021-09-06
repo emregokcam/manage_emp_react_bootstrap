043700f9cf6b8c04ab2acb1e86a676139c1cb0a7
Emre <emregokcam99@gmail.com>
2021-09-06 11:39:47 +0300

    Added files

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Employee from './Employee'
+
+const EmployeeList = () => {
+    return (
+
+        <table className="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Address</th>
+                    <th>Phone</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <Employee />
+            </tbody>
+        </table>
+            )
+}
+
+export default EmployeeList
